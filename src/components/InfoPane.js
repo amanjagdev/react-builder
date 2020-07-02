7d5d5a7d63a2a711650bbadc1350704d4bd7f358
amanjagdev <amankumarjagdev@gmail.com>
2020-07-02 12:17:57 +0530

    Updated Styles of InfoPane

--- src/components/InfoPane.js
+++ src/components/InfoPane.js
@@ -8,20 +8,20 @@ const InfoPane = () => {
             <div className="container-info">
                 <div className="main">Want to create a React App with less no of steps?</div>
 
-                <p>You can create a react app with few simple steps. <br/><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat odio itaque temporibus, nulla minima libero aperiam non? Iste, pariatur excepturi! <br/> <br/>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veritatis. </p>
+                <p>You can create a react app with few simple steps. <br /><br />Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat odio itaque temporibus, nulla minima libero aperiam non? Iste, pariatur excepturi! <br /> <br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veritatis. </p>
 
                 <div className="box">
                     <h4>Instructions</h4>
                     <ul>
-                        <li>Step One Lorem ipsum dolor sit amet.</li>
-                        <li>Step One Lorem ipsum dolor sit amet.</li>
-                        <li>Step One Lorem ipsum dolor sit amet.</li>
-                        <li>Step One Lorem ipsum dolor sit amet.</li>
+                        <li> {' 🔹 '}   Step One Lorem ipsum dolor sit amet.</li>
+                        <li> {' 🔹 '}   Step One Lorem ipsum dolor sit amet.</li>
+                        <li> {' 🔹 '}   Step One Lorem ipsum dolor sit amet.</li>
+                        <li> {' 🔹 '}   Step One Lorem ipsum dolor sit amet.</li>
                     </ul>
                 </div>
-            
+
             </div>
-            <h4>Made with love by Aman Jagdev</h4>
+            <h4 className="madeBy">Made with love by Aman Jagdev</h4>
         </div>
     )
 }
