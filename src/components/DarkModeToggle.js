a019f4f6267a6ebc7b72e806d36812d261f1ea21
Nitin Ranganath <nitinranganath@gmail.com>
2020-10-03 16:35:26 +0530

    Implement the dark mode functionality

--- src/components/DarkModeToggle.js
+++ src/components/DarkModeToggle.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DarkModeToggle = () => {
+  const currentTheme = localStorage.getItem("theme") || "light";
+  const [theme, setTheme] = useState(currentTheme);
+
+  const changeTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+    const themeToSet = theme === "light" ? "dark" : "light";
+    updateTheme(themeToSet);
+  };
+
+  const updateTheme = (theme) => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  };
+
   return (
     <div className="theme-switch-wrapper">
       <span id="toggle-icon">
-        <i className="fas fa-sun"></i>
+        <i className={`fas ${theme === "light" ? "fa-sun" : "fa-moon"}`}></i>
       </span>
       <label className="theme-switch">
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={theme == "dark"}
+          onChange={() => changeTheme()}
+        />
         <div className="slider round"></div>
       </label>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import InfoPane from "./InfoPane";
 import Settings from "./Settings";
 import DarkModeToggle from "./DarkModeToggle";
 
 const Home = () => {
+  useEffect(() => {
+    const currentTheme = localStorage.getItem("theme") || "light";
+    document.documentElement.setAttribute("data-theme", currentTheme);
+  });
+
   return (
     <div className="Home">
       <div className="container">
